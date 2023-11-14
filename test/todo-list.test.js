48a54ff62531d4f5bfe373931e9a6210c68dd0ea
edgee89 <138582912+edgee89@users.noreply.github.com>
2023-11-14 14:51:50 +0000

    UI method fixes and new unit tests

--- test/todo-list.test.js
+++ test/todo-list.test.js
@@ -26,15 +26,95 @@ describe('To-do list functionality', () => {
     expect(todoList.tasks).toEqual([testTask1, testTask2])
   })
 
-  it.skip('should not add a task to a list if the task has no name', () => {
+  it('should not add a task to a list if the task has no name', () => {
     // 🎯 implement test here
+    const toDoList = new TodoList()
+    const testTask = new Task('')
+
+    toDoList.addTask(testTask)
+
+    expect(toDoList.tasks).toEqual([])
   })
 
-  it.skip('should correctly toggle the completion status of a task', () => {
+  it('should correctly toggle the completion status of a task', () => {
     // 🎯 implement test here
+    const testTask = new Task('Laundry')
+
+    expect(testTask.isComplete).toEqual(false)
+
+    testTask.toggleCompletion()
+    expect(testTask.isComplete).toEqual(true)
+
+    testTask.toggleCompletion()
+    expect(testTask.isComplete).toEqual(false)
   })
 
-  it.skip('should correctly delete a task from a to-do list', () => {
+  it('should correctly delete a task from a to-do list', () => {
     // 🎯 implement test here
+    const toDoList = new TodoList()
+    const testTask1 = new Task('Laundry')
+    const testTask2 = new Task('Shopping')
+
+    toDoList.addTask(testTask1, testTask2)
+    toDoList.deleteTask(testTask2)
+    expect(toDoList.tasks).toEqual([testTask1])
   })
+
+  it('should count the correct total number of tasks in a to-do list', () => {
+    const toDoList = new TodoList()
+    toDoList.addTask(new Task('1'))
+    toDoList.addTask(new Task('2'))
+    toDoList.addTask(new Task('3'))
+    const tasksCount = toDoList.countTotalTasks()
+
+    expect(tasksCount).toBe(3)
+  })
+
+  it('should count the correct number of incomplete tasks in a to-do list', () => {
+    const toDoList = new TodoList()
+    toDoList.addTask(new Task('1', true))
+    toDoList.addTask(new Task('2', false))
+    toDoList.addTask(new Task('3', false))
+    const incompleteTasksCount = toDoList.countIncompleteTasks()
+
+    expect(incompleteTasksCount).toBe(2)
+  })
+
+  it('should count the correct number of complete tasks in a to-do list', () => {
+    const toDoList = new TodoList()
+    toDoList.addTask(new Task('1', true))
+    toDoList.addTask(new Task('2', false))
+    toDoList.addTask(new Task('3', true))
+    const completeTasksCount = toDoList.countCompleteTasks()
+
+    expect(completeTasksCount).toBe(2)
+  })
+
+  it('should determine a to-do list is not complete if there are no tasks in it', () => {
+    const toDoList = new TodoList()
+    const isListComplete = toDoList.checkIsEntireListComplete()
+
+    expect(isListComplete).toBe(false)
+  })
+
+  it('should determine a to-do list is not complete if it contains incomplete tasks', () => {
+    const toDoList = new TodoList()
+    toDoList.addTask(new Task('1', false))
+    toDoList.addTask(new Task('2', true))
+    toDoList.addTask(new Task('3', false))
+    const isListComplete = toDoList.checkIsEntireListComplete()
+
+    expect(isListComplete).toBe(false)
+  })
+
+  it('should determine a to-do list is complete if every task in it is complete', () => {
+    const toDoList = new TodoList()
+    toDoList.addTask(new Task('1', true))
+    toDoList.addTask(new Task('2', true))
+    toDoList.addTask(new Task('3', true))
+    const isListComplete = toDoList.checkIsEntireListComplete()
+
+    expect(isListComplete).toBe(true)
+  })
+
 })
